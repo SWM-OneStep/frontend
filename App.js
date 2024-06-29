f6b41319a98dd9228a18e9675ac5286c57ef05de
고병찬 <70642609+byungchanKo99@users.noreply.github.com>
2024-06-30 04:40:14 +0900

    chore: Update project configuration
    - Updated .eslintrc.json
    - Added jsconfig.json

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
-
-export default function App() {
+const App = () => {
   const login = () => {
-    console.log('login')
-    return
-  }
+    console.log('login');
+
+    return;
+  };
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-      <Button title="login" onPress={()=>{login()}}/>
+      <Button
+        title="login"
+        onPress={() => {
+          login();
+        }}
+      />
     </View>
   );
-}
-
+};
+export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
