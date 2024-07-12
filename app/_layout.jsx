f32950fa537148bff9866e707b2634856ceb7d42
고병찬 <70642609+byungchanKo99@users.noreply.github.com>
2024-07-12 16:27:43 +0900

    feat: Create WeeklyCalendar component
    - Set up UI Kitten theming in Expo Router structure
    - Add theme.json for custom UI Kitten theming

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,11 +1,21 @@
 import { Stack } from 'expo-router';
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
+import * as eva from '@eva-design/eva';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
+
+import { default as theme } from '@/theme/theme.json'; // 커스텀 테마 파일
 
 const RootLayout = () => {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <>
+      <IconRegistry icons={[EvaIconsPack]} />
+      <ApplicationProvider {...eva} theme={{ ...eva.light, ...theme }}>
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+      </ApplicationProvider>
+    </>
   );
 };
 
