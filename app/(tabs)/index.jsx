1dc1198636e95c1453c7b4db9f0e8e9438a768d2
earthyoung <studydatawithme@gmail.com>
2024-07-15 21:32:22 +0900

    fix: accessToken 없으면 login screen으로 이동시키기

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -2,24 +2,31 @@ import { View, StyleSheet } from 'react-native';
 import React, { useEffect } from 'react';
 import WeeklyCalendar from '@/components/WeeklyCalendar';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation } from 'expo-router';
 
 const testLoginApi =
   'http://ec2-54-180-249-86.ap-northeast-2.compute.amazonaws.com:8000/auth/test/';
 
 const TodayView = () => {
+  const navigator = useNavigation();
+
   useEffect(() => {
     const testApi = async () => {
-      const accessToken = await AsyncStorage.getItem('accessToken');
-      const response = await fetch(testLoginApi, {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      });
-      if (!response.ok) {
-        await response.json();
+      try {
+        const accessToken = await AsyncStorage.getItem('accessToken');
+        const response = await fetch(testLoginApi, {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+        if (!response.ok) {
+          await response.json();
+        }
+        testApi();
+      } catch (e) {
+        navigator.navigate('index');
       }
-      testApi();
     };
   });
 
