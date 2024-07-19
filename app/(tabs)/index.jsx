6395fd737469f80ed7cf297239d9e6eb9865b3a4
earthyoung <studydatawithme@gmail.com>
2024-07-19 16:02:41 +0900

    fix: merge conflict 해결하면서 잘못 반영된 부분 수정

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,12 +1,12 @@
 import DailyTodos from '@/components/DailyTodos';
 import WeeklyCalendar from '@/components/WeeklyCalendar';
-import { default as React, default as React } from 'react';
+import moment from 'moment';
+import React, { useState } from 'react';
 import { KeyboardAvoidingView, SafeAreaView, StyleSheet } from 'react-native';
 
-const testLoginApi =
-  'http://ec2-54-180-249-86.ap-northeast-2.compute.amazonaws.com:8000/auth/test/';
-
 const TodayView = () => {
+  const [selectedDate, setSelectedDate] = useState(moment());
+
   return (
     <SafeAreaView style={styles.container}>
       <KeyboardAvoidingView>
