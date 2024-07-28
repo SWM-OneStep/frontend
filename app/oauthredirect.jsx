352c9842408e1b175eebeb9dff1d3c243b1b3d33
고병찬 <70642609+byungchanKo99@users.noreply.github.com>
2024-07-29 03:43:00 +0900

    fix: 렌더링 전 함수 실행 에러 해결
    Warning: Cannot update a component (`ForwardRef(BaseNavigationContainer)`) while rendering a different component (`Oauthredirect`).
    에러 해결

--- app/oauthredirect.jsx
+++ app/oauthredirect.jsx
@@ -1,8 +1,11 @@
 import { useRouter } from 'expo-router';
-
+import { useEffect } from 'react';
 const Oauthredirect = () => {
   const router = useRouter();
-  router.replace('(tabs)');
+
+  useEffect(() => {
+    router.replace('(tabs)');
+  });
   return null;
 };
 
