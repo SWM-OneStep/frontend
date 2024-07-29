3124da5a7f3a29ac2790153a749f745118fd9a91
고병찬 <70642609+byungchanKo99@users.noreply.github.com>
2024-07-29 17:57:57 +0900

    feat: API 정리
    파게이트 올라가서 url 파게이트로 바꿈
    api 부분 에러 핸들링 중복되는거 따로 빼고 함수 설명 업데이트함

--- utils/api.js
+++ utils/api.js
@@ -1,4 +1,4 @@
-import { LOCAL_API } from './config';
+import { API_PATH } from './config';
 import axios from 'axios';
 
 const metadata = accessToken => {
@@ -10,31 +10,135 @@ const metadata = accessToken => {
   };
 };
 
+const handleRequest = async request => {
+  try {
+    const response = await request();
+    return response.data;
+  } catch (err) {
+    console.log(err);
+    throw err; // Rethrow the error to handle it further if needed
+  }
+};
+
 export const Api = {
-  fetchTodos: (accessToken, userId) => {
-    return axios
-      .get(`${LOCAL_API.todos}?user_id=${userId}`, metadata(accessToken))
-      .catch(err => {
-        console.log(err);
-      });
+  // get /todos/todo/ 에서 시작날과 끝날 입력 형식은 뭐지 일단 userId만!
+  /**
+   * 서버로부터 사용자의 todo를 받아온다.
+   *
+   */
+  fetchTodos: ({ accessToken, userId }) => {
+    return handleRequest(() =>
+      axios.get(`${API_PATH.todos}?user_id=${userId}`, metadata(accessToken)),
+    );
   },
-  verifyToken: accessToken => {
-    return axios
-      .post(`${LOCAL_API.verifyToken}`, metadata(accessToken))
-      .catch(err => {
-        console.log(err);
-      });
+  /**
+   * 서버에 새로운 할 일을 추가합니다.
+   *
+   * @param {Object} params - 요청에 필요한 매개변수.
+   * @param {string} params.accessToken - 인증을 위한 액세스 토큰.
+   * @param {Object} params.todoData - 추가할 할 일의 데이터.
+   * @param {string} params.todoData.content - 할 일의 내용.
+   * @param {number} params.todoData.categoryId - 할 일의 카테고리 ID.
+   * @param {string} params.todoData.startDate - 할 일의 시작 날짜 (YYYY-MM-DD 형식).
+   * @param {string} params.todoData.endDate - 할 일의 종료 날짜 (YYYY-MM-DD 형식).
+   * @param {number} params.todoData.userId - 할 일과 연관된 사용자 ID.
+   * @param {string} params.todoData.order - 할 일의 순서 문자열.
+   * @param {boolean} params.todoData.isCompleted - 할 일의 완료 여부.
+   * @returns {Promise<Object>} 새로 추가된 할 일 객체를 반환하는 프로미스.
+   * @throws {Error} 요청이 실패할 경우 에러를 던집니다.
+   */
+  addTodo: ({ accessToken, todoData }) => {
+    return handleRequest(() =>
+      axios.post(API_PATH.todos, todoData, metadata(accessToken)),
+    );
   },
-  login: (tokenData, accessToken) => {
-    return axios
-      .post(`${LOCAL_API.login}`, tokenData, metadata(accessToken))
-      .catch(err => {
-        console.log(err);
-      });
+  /**
+   * 이 함수는 todo를 삭제하는 함수입니다.
+   * * 요청 본문 형식:
+   * {
+   *   "todo_id": 0
+   * }
+   *
+   * 반환되는 객체 형식:
+   * @property {number} id - 삭제된 할 일의 ID.
+   * @property {string} content - 삭제된 할 일의 내용.
+   * @property {number} categoryId - 삭제된 할 일의 카테고리 ID.
+   * @property {string} [startDate] - 삭제된 할 일의 시작 날짜 (YYYY-MM-DD 형식).
+   * @property {string} [endDate] - 삭제된 할 일의 종료 날짜 (YYYY-MM-DD 형식).
+   * @property {number} userId - 삭제된 할 일의 사용자 ID.
+   * @property {string} order - 삭제된 할 일의 순서.
+   * @property {boolean} isCompleted - 삭제된 할 일의 완료 여부.
+   */
+  deleteTodo: ({ accessToken, todoId }) => {
+    return handleRequest(() =>
+      axios.request({
+        url: API_PATH.todos,
+        method: 'DELETE',
+        headers: metadata(accessToken),
+        data: { todo_id: todoId },
+      }),
+    );
   },
+  /**
+   * 서버에서 할 일의 정보를 업데이트합니다.
+   *
+   * @param {Object} params - 요청에 필요한 매개변수.
+   * @param {string} params.accessToken - 인증을 위한 액세스 토큰.
+   * @param {Object} params.updateData - 업데이트할 할 일의 데이터.
+   * @param {number} params.updateData.id - 업데이트할 할 일의 ID (필수).
+   * @param {string} [params.updateData.content] - 할 일의 새로운 내용 (선택적).
+   * @param {number} [params.updateData.categoryId] - 할 일의 새로운 카테고리 ID (선택적).
+   * @param {string} [params.updateData.startDate] - 할 일의 새로운 시작 날짜 (YYYY-MM-DD 형식, 선택적).
+   * @param {string} [params.updateData.endDate] - 할 일의 새로운 종료 날짜 (YYYY-MM-DD 형식, 선택적).
+   * @param {string} [params.updateData.order] - 할 일의 새로운 순서 (선택적).
+   * @param {boolean} [params.updateData.isCompleted] - 할 일의 새로운 완료 여부 (선택적).
+
+   * 응답 객체 형식:
+   * @property {number} id - 업데이트된 할 일의 ID.
+   * @property {string} content - 업데이트된 할 일의 내용.
+   * @property {number} categoryId - 업데이트된 할 일의 카테고리 ID.
+   * @property {string} [startDate] - 업데이트된 할 일의 시작 날짜 (YYYY-MM-DD 형식).
+   * @property {string} [endDate] - 업데이트된 할 일의 종료 날짜 (YYYY-MM-DD 형식).
+   * @property {number} userId - 업데이트된 할 일의 사용자 ID.
+   * @property {string} order - 업데이트된 할 일의 순서.
+   * @property {boolean} isCompleted - 업데이트된 할 일의 완료 여부.
+   */
+  updateTodo: ({ accessToken, updateData }) => {
+    return handleRequest(() =>
+      axios.patch(API_PATH.todos, updateData, metadata(accessToken)),
+    );
+  },
+  /**
+   * 서버에 토큰을 검증 요청을 보냅니다.
+   *
+   * 요청 본문 형식:
+   * {
+   *   "token": "your_accessToken_here"
+   * }
+   */
+  verifyToken: token => {
+    return handleRequest(() => axios.post(API_PATH.verifyToken, { token }));
+  },
+  /**
+   * Google 로그인 요청을 서버에 보냅니다.
+   *
+   * @param {Object} tokenData - 로그인에 필요한 토큰 데이터.
+   * @param {string} tokenData.token - Google 인증 토큰.
+   * @param {string} tokenData.deviceToken - 장치 토큰.
+
+   */
+  googleLogin: tokenData => {
+    return handleRequest(() => axios.post(API_PATH.login, tokenData));
+  },
+
   getUserInfo: accessToken => {
-    return axios.get(LOCAL_API.user, metadata(accessToken)).catch(err => {
-      console.log(err);
-    });
+    return handleRequest(() => axios.get(API_PATH.user, metadata(accessToken)));
+  },
+
+  getCategory: (accessToken, userId) => {
+    return axios.get(
+      `${API_PATH.category}?user_id=${userId}`,
+      metadata(accessToken),
+    );
   },
 };
--- utils/config.js
+++ utils/config.js
@@ -1,24 +1,13 @@
-const LOCAL_BASE_URL = 'http://10.0.2.2:8000';
-const BASE_URL = 'http://10.0.2.2:8000';
+const BASE_URL =
+  'http://ec2-43-200-180-86.ap-northeast-2.compute.amazonaws.com:8000';
 
-export const API = {
+export const API_PATH = {
   categories: `${BASE_URL}/todos/category/`,
   todos: `${BASE_URL}/todos/todo/`,
   subTodos: `${BASE_URL}/todos/sub/`,
   refresh: `${BASE_URL}/token/refresh/`,
   verify: `${BASE_URL}/token/verify/`,
   today: `${BASE_URL}/todos/today/`,
-  login: `${BASE_URL}/auth/login/google/`,
-  user: `${BASE_URL}/auth/user/`,
-};
-
-export const LOCAL_API = {
-  categories: `${LOCAL_BASE_URL}/todos/category/`,
-  todos: `${LOCAL_BASE_URL}/todos/todo/`,
-  subTodos: `${LOCAL_BASE_URL}/todos/sub/`,
-  refresh: `${LOCAL_BASE_URL}/token/refresh/`,
-  verify: `${LOCAL_BASE_URL}/token/verify/`,
-  today: `${LOCAL_BASE_URL}/todos/today/`,
   login: `${BASE_URL}/auth/login/google/`,
   user: `${BASE_URL}/auth/user/`,
 };
