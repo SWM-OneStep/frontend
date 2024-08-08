2a1a99e14165ce8dcec8cdcc9531527f4ade6f19
earthyoung <studydatawithme@gmail.com>
2024-08-08 22:39:34 +0900

    fix: Tabbar에 icon 넣기

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,5 +1,14 @@
-import React from 'react';
 import { Tabs } from 'expo-router';
+import React from 'react';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+const todayIcon = ({ color, size }) => (
+  <MaterialIcons name="today" color={color} size={size} />
+);
+
+const inboxIcon = ({ color, size }) => (
+  <MaterialIcons name="inbox" color={color} size={size} />
+);
 
 const TabLayout = () => {
   return (
@@ -8,12 +17,14 @@ const TabLayout = () => {
         name="index"
         options={{
           title: 'Today',
+          tabBarIcon: todayIcon,
         }}
       />
       <Tabs.Screen
         name="inboxView"
         options={{
           title: 'Inbox',
+          tabBarIcon: inboxIcon,
         }}
       />
     </Tabs>
