ea51dd728a965b197aa066008b21377f7681553d
고병찬 <70642609+byungchanKo99@users.noreply.github.com>
2024-08-10 00:29:48 +0900

    fix: 경로 에러 수정
    googleServicesFile 경로가 없다길래 프로젝트 폴더 기준으로 넣어줌
    QueryClient 두개길래 하나 지움

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -6,7 +6,6 @@ import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { Stack } from 'expo-router';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import * as Sentry from '@sentry/react-native';
 
 Sentry.init({
