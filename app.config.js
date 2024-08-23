2545f50a2c4adb502df118ff86990bc423e2d005
고병찬 <70642609+byungchanKo99@users.noreply.github.com>
2024-08-23 21:05:35 +0900

    fix: Update local server url

--- app.config.js
+++ app.config.js
@@ -1,7 +1,5 @@
-const { version } = require('joi');
-
 let Config = {
-  BASE_URL: 'https://10.0.0.2:8000',
+  BASE_URL: 'http://10.0.2.2:8000',
   SENTRY_MODE: 'development',
 };
 
@@ -23,7 +21,7 @@ module.exports = {
     slug: 'onestep',
     version: '1.0.1',
     orientation: 'portrait',
-    icon: './assets/icon.png',
+    icon: './assets/Group 3.png',
     userInterfaceStyle: 'light',
     splash: {
       image: './assets/splash.png',
@@ -39,7 +37,7 @@ module.exports = {
         process.env.GOOGLE_SERVICES_JSON ||
         './android/app/google-services.json',
       adaptiveIcon: {
-        foregroundImage: './assets/adaptive-icon.png',
+        foregroundImage: './assets/Group 3.png',
         backgroundColor: '#ffffff',
       },
       package: 'com.safezone.onestep',
