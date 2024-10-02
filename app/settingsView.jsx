d22847881290398ea92062201b99bd8069b86620
earthyoung <studydatawithme@gmail.com>
2024-10-02 15:15:19 +0900

    fix: rebase conflict 수정

--- app/settingsView.jsx
+++ app/settingsView.jsx
@@ -1,27 +1,27 @@
-import React, { useContext, useState, useEffect } from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
+import { LoginContext } from '@/contexts/LoginContext';
+import * as eva from '@eva-design/eva';
 import {
   ApplicationProvider,
+  Icon,
   IconRegistry,
   Layout,
   List,
   ListItem,
-  Icon,
-  Text,
 } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
-import * as eva from '@eva-design/eva';
-import { LoginContext } from '@/contexts/LoginContext';
 import { useRouter } from 'expo-router';
+import React, { useContext } from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+
 const HomeIcon = props => <Icon {...props} name="star" />;
 
 const renderItemIcon = props => <HomeIcon {...props} />;
 
-const renderItem = ({ item, handlePress }) => (
+const renderItem = ({ item }) => (
   <ListItem
     title={item.title}
     accessoryLeft={renderItemIcon}
-    onPress={() => handlePress(item)}
+    onPress={() => item.handlePress()}
     style={styles.listItem}
   />
 );
@@ -31,21 +31,25 @@ const SettingsView = () => {
     {
       title: '내 정보',
       id: 1,
+      handlePress: () => {},
     },
     {
       title: '언어 변경',
       id: 2,
+      handlePress: () => {},
     },
     {
       title: '문의',
       id: 3,
+      handlePress: () => {
+        router.push('/settingsContactView');
+      },
     },
   ];
 
   const { userId, accessToken } = useContext(LoginContext);
   const router = useRouter();
 
-  const handlePress = item => {};
   return (
     <>
       <IconRegistry icons={EvaIconsPack} />
@@ -55,7 +59,7 @@ const SettingsView = () => {
             <List
               style={styles.list}
               data={data}
-              renderItem={({ item }) => renderItem({ item, handlePress })}
+              renderItem={({ item }) => renderItem({ item })}
             />
           </Layout>
         </SafeAreaView>
