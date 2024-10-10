513bb134384a64e00a4a3acd0eb79010155f6b30
earthyoung <studydatawithme@gmail.com>
2024-10-10 21:41:51 +0900

    chore: 불필요한 코드 제거

--- app/settingsLanguageView.jsx
+++ app/settingsLanguageView.jsx
@@ -1,14 +1,5 @@
 import '@/locales/index';
-import * as eva from '@eva-design/eva';
-import {
-  ApplicationProvider,
-  IconRegistry,
-  IndexPath,
-  Layout,
-  Menu,
-  MenuItem,
-} from '@ui-kitten/components';
-import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import { IndexPath, Layout, Menu, MenuItem } from '@ui-kitten/components';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { SafeAreaView, StyleSheet } from 'react-native';
@@ -29,26 +20,23 @@ const SettingsLanguageView = () => {
 
   return (
     <>
-      <IconRegistry icons={EvaIconsPack} />
-      <ApplicationProvider {...eva} theme={eva.light}>
-        <SafeAreaView style={styles.container}>
-          <Layout style={styles.layout} level="1">
-            <Menu
-              selectedIndex={selectedIndex}
-              onSelect={index => setSelectedIndex(index)}
-            >
-              <MenuItem
-                title={t('views.settingsLanguageView.korean')}
-                onPress={handleKoreanPress}
-              />
-              <MenuItem
-                title={t('views.settingsLanguageView.english')}
-                onPress={handleEnglishPress}
-              />
-            </Menu>
-          </Layout>
-        </SafeAreaView>
-      </ApplicationProvider>
+      <SafeAreaView style={styles.container}>
+        <Layout style={styles.layout} level="1">
+          <Menu
+            selectedIndex={selectedIndex}
+            onSelect={index => setSelectedIndex(index)}
+          >
+            <MenuItem
+              title={t('views.settingsLanguageView.korean')}
+              onPress={handleKoreanPress}
+            />
+            <MenuItem
+              title={t('views.settingsLanguageView.english')}
+              onPress={handleEnglishPress}
+            />
+          </Menu>
+        </Layout>
+      </SafeAreaView>
     </>
   );
 };
